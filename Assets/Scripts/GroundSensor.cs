2a7d4089e778c5d3c035466bfa057425ca7f394e
ainetta12 <ainasanzg@gmail.com>
2023-11-29 09:19:06 +0100

    escenario

---
+++ Assets/Scripts/GroundSensor.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSensor : MonoBehaviour
+{
+    [SerializedFIeld] private float _jumpForce = 5
+
+    // Update is called once per frame
+    void OnTriggerEnter2D()
+    {
+        
+    }
+}
